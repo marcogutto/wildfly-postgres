43cdb6c937e7e534abb744d1391e1a38bd46e8a9
Marco Seabra <marcogutto@gmail.com>
2020-04-08 09:47:24 -0300

    Setup inicial ...

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# wildfly-postgres
+# Wildfly application server with PostgreSQL database driver
+This image contains Wildfly 18.0.1.Final server with PostgreSQL database driver. At the build time or start you have to setup these enviroment variables
+
+DB_HOST - hostname or IP address of database
+DB_PORT - port of database
+DB_NAME - name of the database
+DB_USER - username used for connection to database
+DB_PASS - password user for connection to database The connection string is then created as jdbc:postgresql://$DB_HOST:$DB_PORT/$DB_NAME more details see startWithPostgres.sh
+There is possibility to copy a cli file into $CLI_DIR all the cli files will be run by jboss-cli at startup time.
+
+# Test
+Project can be tested with separate containers or via docker-compose.
+```
+docker network create wildfly
+docker run -d --name postgres -p 5432:5432 --net=wildfly -e POSTGRES_USER=myapp -e POSTGRES_PASSWORD=my-password postgres:9.6.1
+docker run -d --name wildfly -p 8080:8080 --net=wildfly -e DB_HOST=postgres -e DB_NAME=myapp -e DB_USER=myapp -e DB_PASS=my-password marcogutto/wildfly-postgres
+```
+
+Project is available on [GitHub](https://github.com/marcogutto/wildfly-postgres).
---
+++ startWithPostgres.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+if [ ! -f wildfly.started ]; then
+JBOSS_CLI=$WILDFLY_HOME/bin/jboss-cli.sh
+
+function wait_for_server() {
+  until `$JBOSS_CLI -c "ls /deployment" &> /dev/null`; do
+    echo "Waiting"
+    sleep 1
+  done
+}
+
+echo "=> Starting WildFly server"
+$WILDFLY_HOME/bin/standalone.sh -b=0.0.0.0 -c standalone.xml > /dev/null &
+
+echo "=> Waiting for the server to boot"
+wait_for_server
+
+echo "=> Setup Datasource"
+$JBOSS_CLI -c << EOF
+batch
+# Add PostgreSQL driver
+module add --name=org.postgres --resources=/tmp/postgresql-42.2.12.jar --dependencies=javax.api,javax.transaction.api
+/subsystem=datasources/jdbc-driver=postgres:add(driver-name="postgres",driver-module-name="org.postgres",driver-class-name=org.postgresql.Driver)
+# Add the datasource
+data-source add \
+  --jndi-name=$DATASOURCE_JNDI \
+  --name=$DATASOURCE_NAME \
+  --connection-url=jdbc:postgresql://$DB_HOST:$DB_PORT/$DB_NAME \
+  --driver-name=postgres \
+  --user-name=$DB_USER \
+  --password=$DB_PASS \
+  --check-valid-connection-sql="SELECT 1" \
+  --background-validation=true \
+  --background-validation-millis=60000 \
+  --flush-strategy=IdleConnections \
+  --min-pool-size=10 --max-pool-size=100  --pool-prefill=false
+# Execute the batch
+run-batch
+EOF
+
+FILES=$CLI_DIR/*.cli
+for f in $FILES
+do
+  echo "Processing $f file..."
+  $JBOSS_CLI -c --file=$f
+done
+
+echo "=> Shutdown Wildfly"
+$JBOSS_CLI -c ":shutdown"
+
+echo "=> DEPLOY WARs"
+cp ${DEPLOY_DIR}/* ${WILDFLY_HOME}/standalone/deployments/
+
+touch wildfly.started
+fi
+
+echo "=> Start Wildfly"
+$WILDFLY_HOME/bin/standalone.sh -b=0.0.0.0 -c standalone.xml
